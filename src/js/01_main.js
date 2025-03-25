62263fd51dd7878a196ef81b79caaf2f21eaf7d1
aleksandr_gvozdev <wayk5i@yandex.ru>
2025-03-25 21:32:27 +0300

    feat: добавляет блок популярных и свайпер

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -11,7 +11,30 @@ window.addEventListener('resize', () => {
     console.log(window.innerWidth);
     if (window.innerWidth >= 1920) {
         menu.classList.add('main-nav--is-close');
-        menu.classList.remove('main-nav--is-open');
-        console.log('123');        
+        menu.classList.remove('main-nav--is-open');        
     }
 })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        // Опциональные параметры
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        
+        // Адаптивные настройки
+        breakpoints: {
+            // при ширине окна >= 640px
+            390: {
+                slidesPerView: 2,
+                spaceBetween: 230
+            },
+            // при ширине окна >= 1024px
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
+    });
+});
